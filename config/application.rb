023a2c28f3d6dc90a5e0a50b938c076f1dc5e0c0
inoue-ryoki <mth.1021@icloud.com>
2024-02-23 19:58:37 +0900

    日本語向けに設定変更

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,10 @@ module CatHotwire2
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # タイムゾーンをTokyo（日本）にする
+    config.time_zone = "Tokyo"
+    # デフォルトのロケールを日本にする
+    config.i18n.default_locale = :ja
   end
 end
