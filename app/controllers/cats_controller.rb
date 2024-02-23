9dfdc4491dc919a2f82a0c08c6077dc43b2ab57a
inoue-ryoki <mth.1021@icloud.com>
2024-02-23 21:31:01 +0900

    bootstrapで見た目を整える

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -33,7 +33,7 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "Cat was successfully created."
+      redirect_to @cat, notice: "ねこを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -42,7 +42,7 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other
+      redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -51,7 +51,7 @@ class CatsController < ApplicationController
   # DELETE /cats/1
   def destroy
     @cat.destroy
-    redirect_to cats_url, notice: "Cat was successfully destroyed.", status: :see_other
+    redirect_to cats_url, notice: "ねこを削除しました。"
   end
 
   private
